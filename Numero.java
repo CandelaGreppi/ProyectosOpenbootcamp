b7c1db360400d0c301dfb2631ea6e4de1cfae767
candela <candyygrepp@gmail.com>
2022-06-29 18:31:29 +0200

    Proyecto OpenBootcamp

---
+++ Numero.java
@@ -0,0 +1,16 @@
+public class Numero {
+    public static void main(String[] args) {
+        String  numeroIf = "otro";
+
+        if (numeroIf == "positivo"){
+            System.out.println("Es positivo");
+
+        } else if (numeroIf == "negativo") {
+            System.out.println("Es negativo");
+            
+        }else {
+            System.out.println( " es 0");
+        }
+
+    }
+}
---
+++ estacion.java
@@ -0,0 +1,27 @@
+public class estacion {
+
+    public static void main(String[] args) {
+        var estacion = "primavera";
+
+       switch (estacion) {
+           case "verano":
+               System.out.println("Es verano");
+               break;
+
+           case "invierno":
+               System.out.println("Es invierno");
+               break;
+
+           case "primavera":
+               System.out.println("Es primavera");
+               break;
+
+           default:
+               System.out.println("estoy en default");
+
+
+       }
+    }
+
+
+}
