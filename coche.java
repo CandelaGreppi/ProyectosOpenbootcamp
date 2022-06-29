f3bf658e4f0235628800c751ce61eece49f4262c
candela <candyygrepp@gmail.com>
2022-06-29 11:47:11 +0200

    Proyecto OpenBootcamp

---
+++ coche.java
@@ -0,0 +1,23 @@
+public class coche {
+
+    public static void main(String[] args) {
+        Puerta miCoche = new Puerta();
+        miCoche.SumarPuerta();
+        miCoche.SumarPuerta();
+        miCoche.SumarPuerta();
+        System.out.println(miCoche.puerta);
+
+    }
+}
+
+ class Puerta {
+        public int puerta = 0;
+
+        public void SumarPuerta(){
+            this.puerta++;
+        }
+    }
+
+
+
+
